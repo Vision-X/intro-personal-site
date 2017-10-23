ad37b89ce3f3d0f1c09898eb785d58d34c0c9e70
Mike Marlow <michael.s.marlow@gmail.com>
2017-10-23 15:15:37 -0600

    next big push

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 $("#about-btn").on('click', function() {
-  location.href = "about.html"
+  window.open("about.html", "_blank");
+})
+
+$("#contact-btn").on('click', function() {
+  location.href = "mailto:michael.s.marlow@gmail.com";
+})
+
+$("#github").on('click', function() {
+  window.open("https://github.com/Vision-X", "_blank");
+})
+
+$("#linkedin").on('click', function() {
+  window.open("https://www.linkedin.com/in/michael-marlow/", "_blank");
+})
+
+$("#soundcloud").on('click', function() {
+  window.open("https://www.soundcloud.com/mikemarlow", "_blank");
+})
+
+$("#instagram").on('click', function() {
+  window.open("https://instagr.am/mikemarlow", "_blank");
+})
+
+$("#spotify").on('click', function() {
+  window.open("https://open.spotify.com/user/1248950331/playlist/32EMMXehn6gDsSHOlq5r9R", "_blank");
+})
+
+$("#dog").on('click', function() {
+  window.open("https://www.facebook.com/franklin.chandler.10", "_blank");
 })
